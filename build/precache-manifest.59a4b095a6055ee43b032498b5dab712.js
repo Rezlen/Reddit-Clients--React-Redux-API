ca6cb73c103e1e49c248c9fb5d661e589e0d96d5
Rezlen <soheilyreza@yahoo.com>
2022-07-10 10:55:45 +0100

    Build folder exited from gitignore

---
+++ build/precache-manifest.59a4b095a6055ee43b032498b5dab712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d4f760823962526bb94e0425bad63d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa3549205698fdd45909",
+    "url": "/static/css/main.791fefe6.chunk.css"
+  },
+  {
+    "revision": "c261bd2e2220653548e0",
+    "url": "/static/js/2.b5ad368a.chunk.js"
+  },
+  {
+    "revision": "f09bafd8b2e7fe4d8e763cfbd1fdf23b",
+    "url": "/static/js/2.b5ad368a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa3549205698fdd45909",
+    "url": "/static/js/main.54ab57be.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
